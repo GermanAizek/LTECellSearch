76df8f0ca988efbd6780a6b4ffbd8a0a6ccafff0
Герман Семенов <GermanAizek@yandex.ru>
2023-01-29 21:38:57 +0300

    Added main files
    Signed-off-by: Герман Семенов <GermanAizek@yandex.ru>

---
+++ srsran_include.h
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2013-2022 Software Radio Systems Limited
+ * Copyright 2022-2023 Herman Semenov <GermanAizek@yandex.ru>
+ *
+ * This file is part of srsRAN.
+ *
+ * srsRAN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * srsRAN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * A copy of the GNU Affero General Public License can be found in
+ * the LICENSE file in the top-level directory of this distribution
+ * and at http://www.gnu.org/licenses/.
+ *
+ */
+
+#ifndef SRSRAN_INCLUDE_H
+#define SRSRAN_INCLUDE_H
+
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "srsran/srsran.h"
+
+#include "srsran/common/crash_handler.h"
+#include "srsran/phy/rf/rf_utils.h"
+
+#ifndef DISABLE_RF
+#include "srsran/phy/rf/rf.h"
+#endif
+
+#define MHZ 1000000
+#define SAMP_FREQ 1920000
+#define FLEN 9600
+#define FLEN_PERIOD 0.005
+
+#define MAX_EARFCN 1000
+
+#endif // SRSRAN_INCLUDE_H
